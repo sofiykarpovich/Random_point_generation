326974fecc04a4fa5275d9176f9355807471eefc
sofiykarpovich <sofiykarpovich@icloud.com>
2026-03-03 23:28:56 +0300

    The code has been cleaned up (the commented parts of the code have been removed)

--- point.h
+++ point.h
@@ -13,6 +13,7 @@ public:
         gen.seed(rd());
     }
 
+
     void Shake(std::vector<int>& array);
     std::set<std::vector<int>>& GenerationRandomValueOnSpaceCircle(const int space, const int radius, const int count_point);
     void PrintRandomValueOnSpaceCircle(std::set<std::vector<int>>& coordinates_space_n) const;
